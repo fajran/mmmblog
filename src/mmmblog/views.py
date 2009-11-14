bb9928e9e26ce5c842bfc04b0a7796c91bbd64b7
Fajran Iman Rusadi <fajran@gmail.com>
2009-11-14 12:41:09 +0100

    Links and main page.

--- src/mmmblog/views.py
+++ src/mmmblog/views.py
@@ -10,25 +10,40 @@ def _apply_format(content, format):
     else:
         return content
 
+def _get_blog_entries(max=10, min=5):
+    entries = []
+    entries += list(Blog.objects.filter(sticky=True, published=True).order_by('-date'))
+    max -= len(entries)
+    if max < min:
+        max = min
+    entries += list(Blog.objects.filter(sticky=False, published=True).order_by('-date')[0:max])
+
+    for entry in entries:
+        entry.content = _apply_format(entry.content, entry.format)
+
+    return entries
+
+def _get_links(max=10, min=5):
+    links = []
+    links += list(Link.objects.filter(sticky=True, published=True).order_by('-date'))
+    max -= len(links)
+    if max < min:
+        max = min
+    links += list(Link.objects.filter(sticky=False, published=True).order_by('-date')[0:max])
+
+    return links
+
 def blog(request, blog_id=None):
     if blog_id is not None:
         entry = get_object_or_404(Blog, id=blog_id)
         entry.content = _apply_format(entry.content, entry.format)
         return render_to_response('blog_entry.html', {'entry': entry})
     else:
-        max = 10
-        entries = []
-        entries += list(Blog.objects.filter(sticky=True, published=True).order_by('-date'))
-        max -= len(entries)
-        if max < 5:
-            max = 5
-        entries += list(Blog.objects.filter(sticky=False, published=True).order_by('-date')[0:max])
-
-        for entry in entries:
-            entry.content = _apply_format(entry.content, entry.format)
-
+        entries = _get_blog_entries(10)
         return render_to_response('blog.html', {'entries': entries})
 
 def index(request):
-    return HttpResponse()
+    entries = _get_blog_entries(2, 2)
+    links = _get_links(10, 5)
+    return render_to_response('index.html', {'entries': entries, 'links': links})
     
