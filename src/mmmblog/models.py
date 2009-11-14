9bbff76d58433de0dc14393342dad1b2bf81bc0b
Fajran Iman Rusadi <fajran@gmail.com>
2009-11-14 12:34:05 +0100

    Update models.

--- src/mmmblog/models.py
+++ src/mmmblog/models.py
@@ -8,16 +8,23 @@ FORMAT_CHOICES = (
 
 class Blog(models.Model):
     date = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=250)
     content = models.TextField()
-    format = models.CharField(max_length=10, choices=FORMAT_CHOICES)
+    format = models.CharField(max_length=10, choices=FORMAT_CHOICES, default='html')
     published = models.BooleanField(default=True)
     sticky = models.BooleanField(default=False)
+
+    def __unicode__(self):
+        return self.title
 
 class Link(models.Model):
     date = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=250)
-    url = models.URLField()
+    url = models.CharField(max_length=200) # URLField doesn't allow ftp:// !
     extra = models.TextField(blank=True)
     published = models.BooleanField(default=True)
     sticky = models.BooleanField(default=False)
+
+    def __unicode__(self):
+        return self.title
 
