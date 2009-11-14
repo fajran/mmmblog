9638117f52ad5d9509f5c377729f17ecd92f377f
Fajran Iman Rusadi <fajran@gmail.com>
2009-11-14 12:59:21 +0100

    Cleanup urls.py

--- src/mmmblog/urls.py
+++ src/mmmblog/urls.py
@@ -1,21 +1,11 @@
 from django.conf.urls.defaults import *
 from django.conf import settings
 
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # (r'^$', 'django.views.generic.simple.direct_to_template', {'template': 'homepage.html'}),
-    # (r'^\+media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
-    # Example:
-    # (r'^epiweb/', include('epiweb.foo.urls')),
-
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
-    # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
+    (r'^\+media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
     (r'^admin/', include(admin.site.urls)),
     (r'^blog/$', 'mmmblog.views.blog'),
     (r'^blog/(?P<blog_id>\d+)/$', 'mmmblog.views.blog'),
